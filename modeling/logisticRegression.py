981471d674326e44623259ef031ec00973d620d7
liangeric <liangeric321@gmail.com>
2022-10-05 14:03:32 -0400

    Add code that calculates disparity metrics

--- modeling/logisticRegression.py
+++ modeling/logisticRegression.py
@@ -84,31 +84,53 @@ def get_selectionRate(predicted):
 
 # Get race metrics comparing to ideal Y given training data used
 def metric_report(train_data, predicted_labels):
+    metrics = []
     # Metrics for Race
     race_column_names = category_names["Race"]
     for col in race_column_names:
         print(col[:-5] + ":")
         subideal = idealY[train_data[col] == 1]
         sub_predictions = predicted_labels[train_data[col] == 1]
-        print("  Precision=" + str(round(get_precision(subideal,sub_predictions),4)))
-        print("  Recall=" + str(round(get_recall(subideal,sub_predictions),4)))
-        print("  AUC=" + str(round(get_auc(subideal,sub_predictions),4)))
+        precision = round(get_precision(subideal,sub_predictions),4)
+        metrics += [precision]
+        print("  Precision=" + str(precision))
+        recall = round(get_recall(subideal,sub_predictions),4)
+        metrics += [recall]
+        print("  Recall=" + str(recall))
+        auc = round(get_auc(subideal,sub_predictions),4)
+        metrics += [auc]
+        print("  AUC=" + str(auc))
         fpr,fnr = get_FPR_FNR(subideal,sub_predictions)
+        metrics += [round(fpr,4)]
+        metrics += [round(fnr,4)]
         print("  FPR=" + str(round(fpr,4)))
         print("  FNR=" + str(round(fnr,4)))
-        print("  Selection Rate=" + str(round(get_selectionRate(sub_predictions),4)))
+        selection = round(get_selectionRate(sub_predictions),4)
+        metrics += [selection]
+        print("  Selection Rate=" + str(selection))
     # Metrics for Sex
     sex_column_names = category_names["Sex"]
     for col in sex_column_names:
         print(col[:-4]+":")
         subideal = idealY[train_data[col] == 1]
         sub_predictions = predicted_labels[train_data[col] == 1]
-        print("  Precision=" + str(round(get_precision(subideal,sub_predictions),4)))
-        print("  Recall=" + str(round(get_recall(subideal,sub_predictions),4)))
-        print("  AUC=" + str(round(get_auc(subideal,sub_predictions),4)))
+        precision = round(get_precision(subideal,sub_predictions),4)
+        metrics += [precision]
+        print("  Precision=" + str(precision))
+        recall = round(get_recall(subideal,sub_predictions),4)
+        metrics += [recall]
+        print("  Recall=" + str(recall))
+        auc = round(get_auc(subideal,sub_predictions),4)
+        metrics += [auc]
+        print("  AUC=" + str(auc))
         fpr,fnr = get_FPR_FNR(subideal,sub_predictions)
+        metrics += [round(fpr,4)]
+        metrics += [round(fnr,4)]
         print("  FPR=" + str(round(fpr,4)))
         print("  FNR=" + str(round(fnr,4)))
+        selection = round(get_selectionRate(sub_predictions),4)
+        metrics += [selection]
+        print("  Selection Rate=" + str(selection))
         print("  Selection Rate=" + str(round(get_selectionRate(sub_predictions),4)))
     # Metrics for Income
     income_categories = [0, 25000, 50000, 75000, 100000, 150000, 200000, 300000]
@@ -118,15 +140,73 @@ def metric_report(train_data, predicted_labels):
         print("Income between "+str(low_inclusive)+" and "+str(high_exclusive)+":")
         subideal = idealY[(train_data["Income"] >= low_inclusive) & (train_data["Income"] < high_exclusive)]
         sub_predictions = predicted_labels[(train_data["Income"] >= low_inclusive) & (train_data["Income"] < high_exclusive)]
-        print("  Precision=" + str(round(get_precision(subideal,sub_predictions),4)))
-        print("  Recall=" + str(round(get_recall(subideal,sub_predictions),4)))
-        print("  AUC=" + str(round(get_auc(subideal,sub_predictions),4)))
+        precision = round(get_precision(subideal,sub_predictions),4)
+        metrics += [precision]
+        print("  Precision=" + str(precision))
+        recall = round(get_recall(subideal,sub_predictions),4)
+        metrics += [recall]
+        print("  Recall=" + str(recall))
+        auc = round(get_auc(subideal,sub_predictions),4)
+        metrics += [auc]
+        print("  AUC=" + str(auc))
         fpr,fnr = get_FPR_FNR(subideal,sub_predictions)
+        metrics += [round(fpr,4)]
+        metrics += [round(fnr,4)]
         print("  FPR=" + str(round(fpr,4)))
         print("  FNR=" + str(round(fnr,4)))
-        print("  Selection Rate=" + str(round(get_selectionRate(sub_predictions),4)))
+        selection = round(get_selectionRate(sub_predictions),4)
+        metrics += [selection]
+        print("  Selection Rate=" + str(selection))
+
+    return metrics
+
+# Get disparity for different groups above
+def get_disparity(experiment,comparison):
+    # compute disparities
+    disparities = []
+    for i in range(int(len(experiment)/len(comparison))):
+        subExperiment = experiment[i*6:i*6+6]
+        disparities += list(np.round(np.array(subExperiment)/np.array(comparison),4))
+
+    # print out disparities
+    race_column_names = category_names["Race"]
+    for idx in range(len(race_column_names)):
+        col = race_column_names[idx]
+        print(col[:-5] + ":")
+        print("  Precision Disparity=" + str(disparities[0+6*idx]))
+        print("  Recall Disparity=" + str(disparities[1+6*idx]))
+        print("  AUC Disparity=" + str(disparities[2+6*idx]))
+        print("  FPR Disparity=" + str(disparities[3+6*idx]))
+        print("  FNR Disparity=" + str(disparities[4+6*idx]))
+        print("  Selection Rate Disparity=" + str(disparities[5+6*idx]))
+    # Metrics for Sex
+    sex_column_names = category_names["Sex"]
+    for idx in range(len(sex_column_names)):
+        col = sex_column_names[idx]
+        print(col[:-4]+":")
+        print("  Precision Disparity=" + str(disparities[36+6*idx]))
+        print("  Recall Disparity=" + str(disparities[37+6*idx]))
+        print("  AUC Disparity=" + str(disparities[38+6*idx]))
+        print("  FPR Disparity=" + str(disparities[39+6*idx]))
+        print("  FNR Disparity=" + str(disparities[40+6*idx]))
+        print("  Selection Rate Disparity=" + str(disparities[41+6*idx]))
+    # Metrics for Income
+    income_categories = [0, 25000, 50000, 75000, 100000, 150000, 200000, 300000]
+    for idx in range(len(income_categories)-1):
+        low_inclusive = income_categories[idx]
+        high_exclusive = income_categories[idx+1]
+        print("Income between "+str(low_inclusive)+" and "+str(high_exclusive)+":")
+        print("  Precision Disparity=" + str(disparities[54+6*idx]))
+        print("  Recall Disparity=" + str(disparities[55+6*idx]))
+        print("  AUC Disparity=" + str(disparities[56+6*idx]))
+        print("  FPR Disparity=" + str(disparities[57+6*idx]))
+        print("  FNR Disparity=" + str(disparities[58+6*idx]))
+        print("  Selection Rate Disparity=" + str(disparities[59+6*idx]))
         
 
-metric_report(idealX,ideal_predictions)
+experiment_a = metric_report(idealX,ideal_predictions)
 print("------------------------------")
-metric_report(realX,real_predictions)
+experiment_b = metric_report(realX,real_predictions)
+print("------------------------------")
+comparisonGroup = [0.6721,0.8542,0.5634,0.7273,0.1458,0.8079]
+get_disparity(experiment_b,comparisonGroup)
