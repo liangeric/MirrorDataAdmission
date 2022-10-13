80a9238c81bfe9f73415d542ab81d3364b72f4d3
liangeric <liangeric321@gmail.com>
2022-10-12 21:49:23 -0400

    Added option to select what features are in model along with validation set

--- modeling/logisticRegression.py
+++ modeling/logisticRegression.py
@@ -1,4 +1,5 @@
 # Load necessary packages
+from nis import match
 from os import SEEK_END
 import numpy as np
 import pandas as pd
@@ -11,6 +12,7 @@ from sklearn.metrics import confusion_matrix
 
 # set seed
 seed = 12
+np.random.seed(seed)
 
 # Read in appropriate data
 data1 = pd.read_csv("../admissionNew.csv")
@@ -22,12 +24,19 @@ data1["matched"] = matchedData2["Unnamed: 0"]
 # Drop rows that were not able to find matches
 missingReplaceValue = max(data1["matched"])
 data1["matched"] = data1["matched"].replace(missingReplaceValue,np.nan)
+matchedData2["Unnamed: 0"] = matchedData2["Unnamed: 0"].replace(missingReplaceValue,np.nan)
 data1 = data1.dropna()
 matchedData2 = matchedData2.dropna()
 
 # Drop columns that were used for matching
 realData = data1.drop(["matched"], axis = 1)
 idealData = matchedData2.drop(["Unnamed: 0"], axis = 1)
+
+# List of columns to drop that we don't want to include in model
+drop_list = ["Opportunities","Academic Qualification","Non-Academic Qualification",
+             "Diversity"]
+realData = realData.drop(drop_list, axis = 1)
+idealData = idealData.drop(drop_list, axis = 1)
 
 # Seperate data for modeling
 realX = realData.iloc[:,:-1]
@@ -63,9 +72,35 @@ realY = realY.cat.codes
 idealY = idealY.astype("category")
 idealY = idealY.cat.codes
 
+# Set up cross validation
+realX = realX.reset_index(drop = True)
+idealX = idealX.reset_index(drop = True)
+realXStd = realXStd.reset_index(drop = True)
+idealXStd = idealXStd.reset_index(drop = True)
+realY = realY.reset_index(drop = True)
+idealY = idealY.reset_index(drop = True)
+validation_percentage = 0.2
+validation_size = round(len(realX) * validation_percentage)
+validation_idx = np.random.choice(list(range(len(realX))), validation_size, replace = False)
+
+realX_valid = realX.iloc[validation_idx]
+realX_train = realX.drop(validation_idx,axis = 0)
+idealX_valid = idealX.iloc[validation_idx]
+idealX_train = idealX.drop(validation_idx,axis = 0)
+
+realXStd_valid = realXStd.iloc[validation_idx]
+realY_valid = realY.iloc[validation_idx]
+realXStd_train = realXStd.drop(validation_idx,axis = 0)
+realY_train = realY.drop(validation_idx, axis = 0)
+
+idealXStd_valid = idealXStd.iloc[validation_idx]
+idealY_valid = idealY.iloc[validation_idx]
+idealXStd_train = idealXStd.drop(validation_idx,axis = 0)
+idealY_train = idealY.drop(validation_idx, axis = 0)
+
 # Train logistic regression
-log_reg_ideal = LogisticRegression(random_state=seed).fit(idealXStd,idealY)
-log_reg_real = LogisticRegression(random_state=seed).fit(realXStd,realY)
+log_reg_ideal = LogisticRegression(random_state=seed).fit(idealXStd_train,idealY_train)
+log_reg_real = LogisticRegression(random_state=seed).fit(realXStd_train,realY_train)
 
 # Get coefficients
 def get_coefficients():
@@ -80,8 +115,8 @@ def get_coefficients():
         print("  "+name+": "+str(coef))
 
 # Get logistic regression prediction probabilities
-ideal_prediction_probs = pd.DataFrame(log_reg_ideal.predict_proba(idealXStd))
-real_prediction_probs = pd.DataFrame(log_reg_real.predict_proba(realXStd))
+ideal_prediction_probs = pd.DataFrame(log_reg_ideal.predict_proba(idealXStd_valid))
+real_prediction_probs = pd.DataFrame(log_reg_real.predict_proba(realXStd_valid))
 
 # Get predictions based on threshold
 threshold_list = [0.5,0.7]
@@ -121,7 +156,7 @@ def metric_report(train_data, predicted_labels):
     race_column_names = category_names["Race"]
     for col in race_column_names:
         print(col[:-5] + ":")
-        subideal = idealY[train_data[col] == 1]
+        subideal = idealY_valid[train_data[col] == 1]
         sub_predictions = predicted_labels[train_data[col] == 1]
         precision = round(get_precision(subideal,sub_predictions),4)
         metrics += [precision]
@@ -144,7 +179,7 @@ def metric_report(train_data, predicted_labels):
     sex_column_names = category_names["Sex"]
     for col in sex_column_names:
         print(col[:-4]+":")
-        subideal = idealY[train_data[col] == 1]
+        subideal = idealY_valid[train_data[col] == 1]
         sub_predictions = predicted_labels[train_data[col] == 1]
         precision = round(get_precision(subideal,sub_predictions),4)
         metrics += [precision]
@@ -163,14 +198,13 @@ def metric_report(train_data, predicted_labels):
         selection = round(get_selectionRate(sub_predictions),4)
         metrics += [selection]
         print("  Selection Rate=" + str(selection))
-        print("  Selection Rate=" + str(round(get_selectionRate(sub_predictions),4)))
     # Metrics for Income
     income_categories = [0, 25000, 50000, 75000, 100000, 150000, 200000, 300000]
     for idx in range(len(income_categories)-1):
         low_inclusive = income_categories[idx]
         high_exclusive = income_categories[idx+1]
         print("Income between "+str(low_inclusive)+" and "+str(high_exclusive)+":")
-        subideal = idealY[(train_data["Income"] >= low_inclusive) & (train_data["Income"] < high_exclusive)]
+        subideal = idealY_valid[(train_data["Income"] >= low_inclusive) & (train_data["Income"] < high_exclusive)]
         sub_predictions = predicted_labels[(train_data["Income"] >= low_inclusive) & (train_data["Income"] < high_exclusive)]
         precision = round(get_precision(subideal,sub_predictions),4)
         metrics += [precision]
@@ -192,25 +226,28 @@ def metric_report(train_data, predicted_labels):
     # Metric for specific groups
     print("White, Male, Income 200000 to 300000:")
     filter = (train_data["Income"] >= 200000) & (train_data["Income"] < 300000) & (train_data["White_Race"] == 1) & (train_data["Male_Sex"] == 1)
-    subideal = idealY[filter]
+    subideal = idealY_valid[filter]
     sub_predictions = predicted_labels[filter]
-    precision = round(get_precision(subideal,sub_predictions),4)
-    metrics += [precision]
-    print("  Precision=" + str(precision))
-    recall = round(get_recall(subideal,sub_predictions),4)
-    metrics += [recall]
-    print("  Recall=" + str(recall))
-    auc = round(get_auc(subideal,sub_predictions),4)
-    metrics += [auc]
-    print("  AUC=" + str(auc))
-    fpr,fnr = get_FPR_FNR(subideal,sub_predictions)
-    metrics += [round(fpr,4)]
-    metrics += [round(fnr,4)]
-    print("  FPR=" + str(round(fpr,4)))
-    print("  FNR=" + str(round(fnr,4)))
-    selection = round(get_selectionRate(sub_predictions),4)
-    metrics += [selection]
-    print("  Selection Rate=" + str(selection))
+    if len(sub_predictions) != 0:
+        precision = round(get_precision(subideal,sub_predictions),4)
+        metrics += [precision]
+        print("  Precision=" + str(precision))
+        recall = round(get_recall(subideal,sub_predictions),4)
+        metrics += [recall]
+        print("  Recall=" + str(recall))
+        auc = round(get_auc(subideal,sub_predictions),4)
+        metrics += [auc]
+        print("  AUC=" + str(auc))
+        fpr,fnr = get_FPR_FNR(subideal,sub_predictions)
+        metrics += [round(fpr,4)]
+        metrics += [round(fnr,4)]
+        print("  FPR=" + str(round(fpr,4)))
+        print("  FNR=" + str(round(fnr,4)))
+        selection = round(get_selectionRate(sub_predictions),4)
+        metrics += [selection]
+        print("  Selection Rate=" + str(selection))
+    else:
+        print("Warning: Specific group size is 0!")
 
     return metrics
 
@@ -267,9 +304,9 @@ def get_disparity(experiment,comparison):
         
 #get_coefficients()
 print("------------------------------")
-#experiment_a = metric_report(idealX,ideal_predictions)
+#experiment_a = metric_report(idealX_valid,ideal_predictions)
 print("------------------------------")
-#experiment_b = metric_report(realX,real_predictions)
+#experiment_b = metric_report(realX_valid,real_predictions)
 print("------------------------------")
 comparisonGroup = [0.6721,0.8542,0.5634,0.7273,0.1458,0.8079]
 #get_disparity(experiment_b,comparisonGroup)
